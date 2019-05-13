92c97a718f43b064613d727162fbf385ed3e3b92
gabrielgageiro <gabrielgageiro@gmail.com>
2019-05-13 01:25:06 -0300

    Roroso isso

--- index-controller.js
+++ index-controller.js
@@ -1,11 +1,20 @@
 app.controller('indexCtrl', ['$scope', function($scope) {
 
+    $scope.entradaDeDados = {
+        x : '',
+        y: '',
+        raio : '',
+        angulo : '',
+        velocidade : '',
+        direcao : ''
+    }
+
 	$scope.init = function(){
 		console.table('teste');
 	};
 
-	$scope.teste = function(){
-		console.log('oi');
+	$scope.teste = function(bean){
+        console.log(bean);
 	}
 	
 	$scope.init();
