40b5635651e6093e243f477317c0351af62252bf
gabrielgageiro <gabrielgageiro@gmail.com>
2019-06-03 01:57:14 -0300

    separação

---
+++ src/entradadados/entrada-dados-controller.js
@@ -0,0 +1,21 @@
+app.controller('entradaCtrl', function($scope) {
+
+    $scope.entradaDeDados = {
+        x : '',
+        y: '',
+        raio : '',
+        angulo : '',
+        velocidade : '',
+        direcao : ''
+    };
+
+    $scope.init = function(){
+        console.table('teste');
+    };
+
+    $scope.teste = function(bean){
+        console.log(bean);
+    };
+
+    $scope.init();
+});
