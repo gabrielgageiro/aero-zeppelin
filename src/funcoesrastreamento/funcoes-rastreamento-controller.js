7e91544ab8094fa11b46d5f3ace90723d1d91e60
gabrielgageiro <gabrielgageiro@gmail.com>
2019-06-04 14:06:16 -0300

    Ajuste no tamanho do radar

---
+++ src/funcoesrastreamento/funcoes-rastreamento-controller.js
@@ -0,0 +1,21 @@
+app.controller('rastreamentoCtrl', function($scope) {
+
+    $scope.entradaDeDados = {
+        x : '',
+        y: '',
+        raio : '',
+        angulo : '',
+        velocidade : '',
+        direcao : ''
+    };
+
+    $scope.init = function(){
+        console.table('teste');
+    };
+
+    $scope.teste = function(bean){
+        console.log(bean);
+    };
+
+    $scope.init();
+});
