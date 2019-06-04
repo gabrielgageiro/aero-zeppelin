9f049c1cba2d4a8288fe698958c3316d661dd4da
Cassio <cassio_qwe@outlook.com>
2019-06-04 17:41:52 -0300

    OI 	modified:   README.md 	modified:   index.html 	modified:   src/entradadados/entrada-dados-controller.js 	modified:   src/entradadados/entrada-dados.html

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # aero-zeppelin
 Trabalho de simulação de radar aéreo
 
-1. npm install -g install http-server (Pode ser necessário executar como sudo)
-1. npm install na raiz do projeto.
+1. npm install
+2. npm install -g install http-server (Pode ser necessário executar como sudo)
 3. npm start
+
+
+Referências
+https://developer.mozilla.org/pt-BR/docs/Web/Guide/HTML/Canvas_tutorial/Drawing_shapes
--- src/entradadados/entrada-dados-controller.js
+++ src/entradadados/entrada-dados-controller.js
@@ -13,8 +13,31 @@ app.controller('entradaCtrl', function($scope) {
         console.table('teste');
     };
 
-    $scope.teste = function(bean){
-        console.log(bean);
+    $scope.inserirAviao = function(bean){
+        $scope.carregaAviao(bean);
+    };
+
+    $scope.carregaAviao = function(bean){
+        if($scope.validarPosicaoAviao(bean)){
+        var radar = document.querySelector("#radar");
+
+            var ctx = radar.getContext('2d');
+    
+            //ctx.fillRect(bean.x, bean.y, 5, 3);//x, y, width, height
+            var retangulo = new Path2D();
+            retangulo.rect(bean.x, bean.y, 8, 6);
+            ctx.fill(retangulo);
+        }else {
+            console.log('Já existe um avião proximo, insira-o em outras coordenadas.');
+        }
+    };
+
+    $scope.validarPosicaoAviao = function(bean){
+        console.log($scope.distanciaEntrePontos(30, 36, 33, 40));
+    };
+    
+    $scope.distanciaEntrePontos = function(x1, y1, x2, y2){
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1)), 2);
     };
 
     $scope.init();
