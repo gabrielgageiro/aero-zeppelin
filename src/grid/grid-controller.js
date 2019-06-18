de9eb76fc94b3b2a08ac565c9460942091db59f8
gabrielgageiro <gabrielgageiro@gmail.com>
2019-06-17 22:47:37 -0300

    novo stylo

--- src/grid/grid-controller.js
+++ src/grid/grid-controller.js
@@ -1,7 +1,31 @@
-app.controller('gridCtrl', function($scope) {
+app.controller('gridCtrl', function($scope, $mdDialog) {
 
     $scope.init = function(){
-        console.log('oi');
+    };
+
+
+    $scope.getTesteAviao = function(){
+        var aviao = [];
+
+        for (var i = 0; i< 13; i++){
+            aviao.push("AVIÃO LATAM MATRICULA " + i);
+        }
+        return aviao;
+    };
+
+    $scope.removerAviao = function (index) {
+        var confirm = $mdDialog.confirm()
+            .title('Remover avião ? Risco de acidente!')
+            .textContent('Removendo o aviao, ele não será monitorado e cairá.')
+            .ariaLabel('Remover')
+            .ok('Remover')
+            .cancel('Cancelar');
+
+        $mdDialog.show(confirm).then(function() {
+            console.log("removeu");
+        }, function() {
+            console.log("Cancelou");
+        });
     };
 
 
